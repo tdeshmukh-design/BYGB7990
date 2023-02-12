e438cae5033228d99924fb1949f61103c1143ae4
Tanuja <tdeshmukh@fordham.edu>
2023-02-12 15:38:01 -0500

    time.py fixed indentation

--- time.py
+++ time.py
@@ -1,18 +1,18 @@
 seconds = input("Please  enter seconds as integer number:")
-        while (not seconds.isnumeric() ):
-           seconds = input("Enter a number:")
+while (not seconds.isnumeric() ):
+   seconds = input("Enter a number:")
 
-           seconds = int(seconds)
-           seconds_in_hour = 3600
-           seconds_in_minute = 60
+   seconds = int(seconds)
+   seconds_in_hour = 3600
+   seconds_in_minute = 60
 
-           # Calculare hours and leftover seconds
-           hours = seconds // seconds_in_hour
-           seconds = seconds % seconds_in_hour
+   # Calculare hours and leftover seconds
+   hours = seconds // seconds_in_hour
+   seconds = seconds % seconds_in_hour
 
-           # Calculare minutes and leftover seconds
-           minutes = seconds // seconds_in_minute
-           seconds = seconds % seconds_in_minute
+   # Calculare minutes and leftover seconds
+   minutes = seconds // seconds_in_minute
+   seconds = seconds % seconds_in_minute
 
-           print ("%d hours, %d minutes, %d seconds \n" % (hours, minutes, seconds))
+   print ("%d hours, %d minutes, %d seconds \n" % (hours, minutes, seconds))
 
